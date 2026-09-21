53643c3e1b45a916d3c96d507e6e4cf113921bc3
Theo Schlestein <theo.schlestein@gmail.com>
2026-09-21 10:24:30 +0200

    erste kalender ansicht

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <script setup>
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+
+const calendarOptions = {
+  plugins: [dayGridPlugin],
+  initialView: 'dayGridMonth',
+  events: [
+    { title: 'Erster Termin', start: '2026-09-22' },
+    { title: 'Zweiter Termin', start: '2026-09-25' }
+  ]
+}
 </script>
 
 <template>
-  <h1>Mein Gruppen-Kalender</h1>
+  <FullCalendar :options="calendarOptions" />
 </template>
